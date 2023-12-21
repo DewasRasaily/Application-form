2532fb22c6f5e57e855f4362c75cd8ee437b1063
dewas <dwas.rasaily92@gmail.com>
2023-12-21 17:15:33 +0600

    first

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { LeaveformComponent } from './leaveform/leaveform.component';
+
+
+export const routes: Routes = [
+    { path: '', component: LeaveformComponent },
+];
---
+++ src/app/leaveform/leaveform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leaveform',
+  standalone: true,
+  imports: [],
+  templateUrl: './leaveform.component.html',
+  styleUrl: './leaveform.component.css'
+})
+export class LeaveformComponent {
+
+}
